dd44ee2553cef73deed2dce953070930070521e6
Jonathan Olivera <jonathanso.2002@gmail.com>
2024-09-26 12:57:28 -0300

    ...

---
+++ src/componnets/Header.jsx
@@ -0,0 +1,14 @@
+import Style from "./Header.module.css"
+
+function Header(){
+    return(
+        <>
+            <div className ={Style.cabecalho}>
+                <h1>Header da pagina</h1>
+            </div>
+       </>
+        
+
+    )
+}
+export default Header
---
+++ src/componnets/Meio.jsx
@@ -0,0 +1,13 @@
+import './Meio.module.css'
+function Meio(){
+    return(
+        <>
+        <div classname= "cabecalho">
+            <h1>Meio da pagina</h1>
+        </div>
+       </>
+        
+
+    )
+}
+export default Meio
